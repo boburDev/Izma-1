7d6ac83b6f85fc153cc054e490de4dbaa3722101
boburDev <soffit021@gmail.com>
2021-09-30 19:14:58 +0500

    Update StudentsTable.jsx

--- src/pages/Students/containers/StudentsTable/StudentsTable.jsx
+++ src/pages/Students/containers/StudentsTable/StudentsTable.jsx
@@ -1,33 +1,23 @@
-import {  Drawer } from 'antd';
+import {  Drawer } from 'antd'
 import Table from '../../../../components/Table/Table'
-import { connect } from "react-redux";
-import DeleteImg from '../../../../assets/Icons/delete-border.svg'
-import DollarImg from '../../../../assets/Icons/dollar-border.svg'
-// import { setMainTableData, setRowId, setValues } from '../../ActionRedux/IzmaActions';
-import { Link } from 'react-router-dom'
 import './StudentsTable.scss'
-import { useEffect, useState } from 'react';
-import FinanceAddPaymentForm from '../../../../containers/Finances/FinancesForm/FinanceAddPaymentForm/financeAddPaymentForm';
-import { useMutation, useQuery, useSubscription } from '@apollo/client';
-import { ALL_STUDENTS, DELETE_STUDENT, SUBSCRIPTION, STUDENT_ON_KEY_UP, STUDENT_COUNT, FIND_SALE, FILTER_COURSE } from '../../../../Querys/Table_Query';
-import { Modal } from 'antd';
-import Check from '../../../../components/Check/Check';
-// import { useCourse } from '../filterSoha/context'
+import { useEffect, useState } from 'react'
+import FinanceAddPaymentForm from '../../../../containers/Finances/FinancesForm/FinanceAddPaymentForm/financeAddPaymentForm'
+import { useMutation, useQuery, useSubscription } from '@apollo/client'
+import { ALL_STUDENTS, DELETE_STUDENT, SUBSCRIPTION, STUDENT_ON_KEY_UP, STUDENT_COUNT, FIND_SALE, FILTER_COURSE } from '../../../../Querys/Table_Query'
+import { Modal } from 'antd'
+import Check from '../../../../components/Check/Check'
 import { useCheck } from '../../../../context/CheckProvider'
-import { useStudentData } from '../../../../context/StudentTableProvider';
-import { useStudentPay } from '../../../../context/StudentPay';
-import { useStudentFilter } from '../../../../context/StudentFilter';
-import { useCourseFilter } from '../../../../context/CourseFilterProvider';
-import { useLoader } from '../../../../context/Loader';
-import { usePagination } from '../../../../context/Pagination';
+import { useStudentData } from '../../../../context/StudentTableProvider'
+import { useStudentPay } from '../../../../context/StudentPay'
+import { useStudentFilter } from '../../../../context/StudentFilter'
+import { useCourseFilter } from '../../../../context/CourseFilterProvider'
+import { useLoader } from '../../../../context/Loader'
+import { usePagination } from '../../../../context/Pagination'
 
 
-<<<<<<< HEAD
-const StudentsTable = ({ setRowId, setValues, studentSearch }) => {
-   const [page] = usePagination()
-=======
 const StudentsTable = ({ studentSearch }) => {
->>>>>>> e33527c5a42bce0f4aca9a7d0f0d0dfc5f321555
+   const [page] = usePagination()
    const [deb] = useStudentFilter()
    const [courseFilter] = useCourseFilter()
    const [setData] = useStudentData(true)
@@ -41,44 +31,43 @@ const StudentsTable = ({ studentSearch }) => {
       }
    })
 
-   console.log(page?.page);
-   console.log(page?.count);
+   // console.log(page?.page)
+   // console.log(page?.count)
 
 
 
-
-   const { data: ddd } = useQuery(STUDENT_ON_KEY_UP, { variables: { name: studentSearch } })
+   const { data: studentOnKeyUp } = useQuery(STUDENT_ON_KEY_UP, { variables: { name: studentSearch } })
    const { data: countSt } = useQuery(STUDENT_COUNT, { variables: { count: +page?.count } })
    const [getID] = useMutation(DELETE_STUDENT)
 
-   useEffect(() => {
 
-      const searchedStudent = ddd && ddd.studentOnKeyup.map((i, index) => {
-         return { ID: i.id, id: index + 1, name: i.name, phoneNumber: i.mainPhone, date: i.groups, status: i.status }
-      })
+   useEffect(() => {
+      const searchedStudent = studentOnKeyUp && studentOnKeyUp.studentOnKeyup
+
+      console.log(deb)
+      console.log(searchedStudent)
+      console.log(students && students.students)
 
       if (students && students.students) {
          const users = students && students.students.map((i, index) => {
             return { ID: i.id, id: index + 1, name: i.name, phoneNumber: i.mainPhone, date: i.groups, status: i.status }
          })
 
-
-
          if (deb) {
             const filterStatus = users.filter(item => item.status === 4)
             setUserData(filterStatus)
          }
-         else if (searchedStudent) {
+         else if (searchedStudent.length) {
 
             setUserData(searchedStudent)
          } else {
 
-            setUserData(users)
+            setUserData(students && students.students)
          }
 
       }
 
-   }, [students, deb, studentSearch, ddd])
+   }, [students, deb, studentSearch, studentOnKeyUp])
 
 
 
@@ -92,11 +81,11 @@ const StudentsTable = ({ studentSearch }) => {
 
 
    const showDrawer = () => {
-      setVisible(true);
-   };
+      setVisible(true)
+   }
    const onClose = () => {
-      setVisible(false);
-   };
+      setVisible(false)
+   }
 
 
    useEffect(() => {
@@ -127,15 +116,15 @@ const StudentsTable = ({ studentSearch }) => {
 
 
 
-   const [isModalVisible, setIsModalVisibleY] = useState(false);
+   const [isModalVisible, setIsModalVisibleY] = useState(false)
 
    const handleOkY = () => {
-      setIsModalVisibleY(false);
-   };
+      setIsModalVisibleY(false)
+   }
 
    const handleCancelY = () => {
       setIsModalVisibleY(false)
-   };
+   }
 
 
    return (

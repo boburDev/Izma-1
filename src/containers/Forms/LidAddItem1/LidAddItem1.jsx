540c8ce03c05d43dd5a29ac16776e27dbe0c5a9b
Asadullo <asadullosadirdinov@gmail.com>
2021-10-05 06:08:26 +0500

    lids5

--- src/containers/Forms/LidAddItem1/LidAddItem1.jsx
+++ src/containers/Forms/LidAddItem1/LidAddItem1.jsx
@@ -1,10 +1,15 @@
 import './LidAddItem1.scss'
-import { useRef } from 'react'
+import { useRef , useState } from 'react'
 import request from '../../../services'
+import DropSearch from '../../../components/DropSearch/DropSearch'
+import { TimePicker } from 'antd'
+import moment from 'moment';
 
+const format = 'HH:mm';
 
 const LidAddItem1 = ({ setActive1, active1, refresh, setRefresh, onClose }) => {
    const itemName = useRef()
+   const [time, setTime] = useState('')
    // const boardIn = useRef()
 
 
@@ -31,19 +36,25 @@ const LidAddItem1 = ({ setActive1, active1, refresh, setRefresh, onClose }) => {
             </div>
             <div className="row">
                <label htmlFor="Nomi">Kursni tanlash</label>
-               <input type="text" ref={itemName} />
+               <DropSearch
+                  pInput="Kursni tanlang"
+               />
             </div>
             <div className="row">
                <label htmlFor="Nomi">O’qituvchini tanlang</label>
-               <input type="text" ref={itemName} />
+               <DropSearch
+                  pInput="O’qituvchini tanlang"
+               />
             </div>
             <div className="row">
                <label htmlFor="Nomi">Kunlar</label>
-               <input type="text" ref={itemName} />
+               <DropSearch
+                  pInput="Kunlarni tanlang"
+               />
             </div>
             <div className="row">
                <label htmlFor="Nomi">Darsning boshlanish vaqti</label>
-               <input type="text" ref={itemName} />
+               <TimePicker onChange={e => setTime(e.format('HH:mm'))} defaultValue={moment('00:00', format)} format={format} />
             </div>
             <button type="submit" >Yaratish</button>
          </form>

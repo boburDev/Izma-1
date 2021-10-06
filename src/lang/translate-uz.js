6f7eebb8d590a6358c9a72a342a1a585702b922b
shon_parker <nizamkhujaev@gmail.com>
2021-10-06 21:35:58 +0500

    finished uzb language

--- src/lang/translate-uz.js
+++ src/lang/translate-uz.js
@@ -35,7 +35,521 @@ const uz = {
             leads: 'lidlar',
             waitAndPack: 'Kutish va to\'plam',
             groupDebpt: 'Guruhdagi qarz'
+        },
+        journals: {
+            courseStarted: 'Kurs ochilgan:'
+        },
+        tabels: {
+            oddDays: 'Toq kunlar',
+            evenDays: 'Juft kunlar',
+            weekends: 'Dam olish kunlari',
+            everyDay: 'Har kuni',
+            others: 'Boshqalar'
         }
+    },
+    students: {
+        title: {
+            pageTitle: 'O\'quvchilar'
+        },
+        search: {
+            searchHolder: 'Ism yoki telefon raqam orqali qidirish'
+        },
+        addNewStudent: {
+            addStudent: 'Yangi o\'quvchi qo\'shish'
+        },
+        abtCourse: {
+            debt: 'Qarzdorlar',
+            discount: 'Chegirmalar'
+        },
+        id: {
+            fullName: 'F.I.O',
+            phoneNumber: 'Telefon',
+            groupNames: 'Guruhlar'
+        },
+        studentInfoTitle: {
+            students: 'O\'quvchilar',
+            profile: 'Profil',
+            menu: 'Menu'
+        },
+        archiveStudents: {
+            archiveStudent: 'Profil Tarix'
+        },
+        studentsInfo: {
+            studentPhoneNumber: 'Telefon raqam:',
+            studentBalance: 'Balans:',
+            role: 'Rol:',
+            fillial: 'Filial:'
+        },
+        groups: {
+            groupsTitle: 'Guruhlar',
+            groupsSelected: 'Kurs tanlanadi'
+        },
+        payment: {
+            paymentTitle: 'TO\'LOVLAR',
+            recordedDate: 'Qayd etilgan sana',
+            paymentId: 'ID',
+            paymentDate: 'Sana',
+            debt: 'Qarz',
+            credit: 'Kredit',
+            comment: 'Izoh',
+            employee: 'Xodim'
+        },
+        background: {
+            data: 'Ma\'lumotlar yo\'q',
+            addNewNote: 'Yangi eslatma qo\'shish',
+            comment: 'Izoh',
+            save: 'Saqlash'
+        },
+        editStudentInfo: {
+            editStudentInfoTitle: 'O\'QUVCHI MA\'LUMOTLARINI TAHRIRLASH',
+            phoneNumber: 'Telefon raqam',
+            fullName: 'F.I.O',
+            birthday: 'Tug\'ilgan sana',
+            gender: ['Erkak', 'Ayol'],
+            comment: 'Izoh',
+            secondaryPhone: 'Ikkilamchi telefon',
+            parentPhoneNumber: 'Ota-onasining telefon raqami',
+            telegramUsername: 'Telegram(@username)',
+            password: 'Parol',
+            edit: 'O\'zgartirish'
+        },
+        sendMessageToStudent: {
+            message: 'Sizning o\'quv markazingiz uchun faollashtirilmagan!'
+        },
+        recordPayment: {
+            recordPaymentTitle: 'To\'lovlarni qayd etish',
+            fullName: 'F.I.O',
+            paymentType: 'To\'lov usuli:',
+            cash: 'Naqd  pul',
+            card: 'UzCard',
+            bankAccount: 'Bank hisobi orqali',
+            acceptedPaymentValue: 'Qabul qilingan pul miqdori:',
+            groupName: 'Guruh',
+            AcceptedDate: 'Qabul qilingan sana',
+            save: 'Saqlash',
+            accountReplenished: 'Hisob to\'ldirildi:',
+            situationEdited: 'Holati o\'zgartirildi: Harakatsiz (Sinov darsi)'
+        },
+        addNewStudentTitle: {
+            addNewStudentTitle: 'YANGI O\'QUVCHI QO\'SHISH',
+            phoneNumber: 'Telefon raqam',
+            fullName: 'F.I.O',
+            birthday: 'Tug\'ilgan sana',
+            gender: ['Erkak', 'Ayol'],
+            selectGroup: 'Guruhni tanlang',
+            comment: 'Izoh',
+            secondaryPhone: 'Ikkilamchi telefon',
+            parentPhoneNumber: 'Ota-onasining telefon raqami',
+            telegramUsername: 'Telegram (@username)',
+            password: 'Parol',
+            save: 'Saqlash'
+        }
+    },
+    teachers: {
+        teacher: {
+            teacherTitle: 'O\'QITUVCHILAR'
+        },
+        teacherInfo: {
+            teacherId: 'ID',
+            fullName: 'F.I.O',
+            phoneNumber: 'Telefon raqami'
+        }, 
+        addNewUser: {
+            phoneNumber: 'Telefon raqami',
+            fullName: 'F.I.O',
+            birthday: 'Tug\'ilgan sana',
+            gender: ['Erkak', 'Ayol'],
+            comment: 'Izoh',
+            photo: 'Foto',
+            password: 'Parol',
+            save: 'Saqlash'
+        },
+        teacherInfo: {
+            teacherName: 'O\'qituvchining Ismi',
+            archiveProfile: 'PROFIL TARIX',
+            phoneNumber: 'Telefon raqam:',
+            birthday: 'Tug\'ilgan sana:',
+            role: 'Rol:',
+            fillial: 'Filial:'
+        },
+        groups: {
+            groupName: 'Guruh nomi',
+            groupdCode: 'Kodi',
+            date: 'Sana, vaqt'
+        },
+        courses: {
+            courseName: 'Kurs nomi',
+            startDay: 'Boshlash: (kurs vaqti)',
+            profileHistory: 'Profil Tarix',
+            accountReplenished: 'Hisob to\'ldirildi:',
+            editSituation: 'Holati o\'zgartirildi: Harakatsiz (Sinov darsi)'
+        },
+        editTeacherInfo: {
+            editTeacherInfoTitle: 'O\'QITUVCHI MA\'LUMOTLARINI TAHRIRLASH',
+            phoneNumber: 'Telefon raqami',
+            fullName: 'F.I.O',
+            birthday: 'Tug\'ilgan sana',
+            gender: ['Erkak', 'Ayol'],
+            comment: 'Izoh',
+            photo: 'Foto',
+            fileNotChoosen: 'Hech qanday fayl tanlanmadi',
+            download: 'Yuklash',
+            password: 'Parol',
+            edit: 'O\'zgartirish'
+        }
+    },
+    settings: {
+        map: {
+            mapTitle: 'XARITA',
+            overallBall: 'Umumiy ball',
+            highestBall: 'Yuqori ball',
+            yourBall: 'Sizning ballingiz',
+            section: 'Bo\'lim',
+            questions: 'Savollar',
+            highestScore: 'Ball (yuqori)',
+            result: 'Natija'
+        },
+        employee: {
+            employeeTitle: 'XODIMLAR',
+            addNewEmployee: 'Yangi xodim qo\'shish',
+            id: 'ID',
+            fullName: 'F.I.O',
+            role: 'Roli',
+            phoneNumber: 'Telefon raqami',
+            edit: 'Tahrirlash',
+            actions: 'Amallar'
+        },
+        editable: {
+            editableTitle: 'TAHRIRLASH',
+            phoneNumber: 'Telefon raqami',
+            fullName: 'F.I.O',
+            birthday: 'Tug\'ilgan sanasi',
+            gender: ['Erkak', 'Ayol'],
+            photo: 'Foto',
+            photoNotChoosen: 'Hech qanday rasm tanlanmadi',
+            download: 'Yuklash',
+            password: 'Parol',
+            leader: 'Rahbar',
+            adminstrator: 'Adminstrator',
+            teacher: 'O\'qituvchi',
+            marketolog: 'Marketolog',
+            accountant: 'Hisobchi',
+            save: 'Saqlash'
+        },
+        rooms: {
+            roomsTitle: 'XONALAR',
+            addNewRoom: 'Yangi xona qo\'shish',
+            id: 'ID',
+            fullName: 'F.I.O',
+            edit: 'Tahrirlash',
+            actions: 'Amallar',
+            roomName: 'Xona nomi',
+            save: 'Saqlash'
+        },
+        journals: {
+            journalsTitle: 'JURNALLAR'
+        },
+        forms: {
+            formTitle: 'FORMALAR',
+            addNewForm: 'Yangi forma yaratish',
+            id: 'ID',
+            fullName: 'F.I.O',
+            role: 'Roli',
+            phoneNumber: 'Telefon raqami',
+            actions: 'Amallar',
+            messageForLc: 'O\'quv markazimizga savol va murojaatlaringizni qoldiring.',
+            happyWhatWelistening: 'Sizni eshitishdan xursandmiz!',
+            surname: 'Familiya',
+            name: 'Ism',
+            phoneNum: 'Telefon raqam',
+            comment: 'Izoh',
+            selectCourse: 'Kursni tanlang',
+            send: 'Yuborish',
+            commentedForLc: 'O\'quv markaziga so\'rovlaringizni qoldirishingiz mumkin.',
+            enter: 'Kirish',
+            numberPhone: 'Telefon raqami',
+            password: 'Parol',
+            download: 'Yuklash',
+            enterence: 'Kirish'
+        },
+        companySettings: {
+            companySettingsTitle: 'KOMPANIYA SOZLAMALARI',
+            logo: 'Logotip',
+            chooseMainColor: 'Asosiy rangni tanlang',
+            lcName: 'O\'quv markazining nomi',
+            full: 'To\'liq',
+            bgcForPage: 'Sahifa foni uchun rasm',
+            download: 'Yuklash',
+            mainPhoto: 'Asosiy rasm',
+            suggestions: 'Tashkilot takliflari',
+            paymentTypeForStudent: 'Talaba uchun to\'lov rejimi',
+            plasticCard: 'Plastik karta',
+            cash: 'Naqd',
+            acrossBankAccount: 'Bank hisobi orqali',
+            facebookPixel: 'Facebook piksel',
+            paynet: 'Paynet',
+            paycomUserId: 'Paycom foydalanuvchisining ID raqami',
+            paycomeUsername: 'Paycom foydalanuvchi nomi',
+            paycomPassword: 'Paycom parol',
+            save: 'Saqlash'
+        }
+    }, 
+    courses: {
+        courseTitle: 'KURSLAR',
+        addCourse: 'Yangi qo\'shish',
+        editCourse: {
+            editCourseTitle: 'KURSNI TAHRIRLASH',
+            name: 'Nomi',
+            price: 'Narxi',
+            description: 'Tavsif',
+            create: 'Yaratish'
+        },
+        courseName: {
+            courseNameTitle: 'KURS NOMI',
+            onlineLessonMaterials: 'Online dars va materiallar',
+            degrees: 'Darajalar',
+            groups: 'Guruhlar'
+        },
+        addLesson: {
+            addLessonTitle: 'Dars qo\'shish',
+        },
+        degrees: {
+            degreesTitle: 'Darajalar',
+            addDegree: 'Daraja qo\'shish',
+            notDegree: 'Darajalar hozircha kiritilmagan.'
+        }
+    },
+    navigation: {
+        main: 'Asosiy',
+        lids: 'Lidlar',
+        students: 'O\'quvchilar',
+        teachers: 'O\'qituvchilar',
+        courses: 'Kurslar',
+        finance: 'Moliya',
+        settings: 'Sozlamalar',
+        allPayments: 'Barcha to\'lovlar',
+        costs: 'Xarajatlar',
+        salaryForWork: 'Ish haqi',
+        paymentGroups: 'To\'lovlar guruhi',
+        paymentCourses: 'Kurs to\'lovlari',
+        map: 'Xarita',
+        office: 'Ofis',
+        others: 'Boshqalar',
+        shapes: 'Shakllar',
+        companySettings: 'Kompaniya sozlamalari'
+    },
+    finance: {
+        financeTitle: 'MOLIYA (Barcha o\'quvchilar)',
+        countStudentOverSum: 'O\'quvchilar hisobi bo\'yicha jami:  so\'m',
+        receiptsForPeriod: 'Davr uchun tushumlar:',
+        currency: 'so\'m',
+        startDay: 'Sanadan boshlab',
+        endDay: 'Sanagacha',
+        filter: {
+            filterTitle: 'Filtr',
+            studentsFullName: 'Talabalar F.I.O',
+            phoneNumber: 'Telefon raqami',
+            typePayment: 'To\'lov turi',
+            currency: 'So\'m',
+            comment: 'Izoh'
+        },
+        costs: {
+            costsTitle: 'XARAJATLAR',
+            startDay: 'Sanadan boshlab',
+            endDay: 'Sanagacha',
+            filter: 'Filtr'
+        },
+        costsForPeriod : {
+            costsForPeriodTitle: 'Davr uchun xarajat puli',
+            name: 'Nomi',
+            date: 'Sana',
+            type: 'Turkum',
+            recipient: 'Oluvchi',
+            currency: 'So\'m',
+            actions: 'Amallar'
+        },
+        newCosts: {
+            newCostsTitle: 'Yangi xarajatlar',
+            name: 'Nomi',
+            recievedDate: 'Qabul qilingan sana',
+            yymmdd: 'kun/oy/yil',
+            type: 'Turkum',
+            others: 'Boshqalar',
+            recipient: 'Oluvchi',
+            currency: 'So\'m',
+            save: 'Saqlash'
+        },
+        select: {
+            selectTitle: 'Tanlash',
+            administrativeExpenses: 'Ma\'muriy xarajatlar',
+            rent: 'Ijara',
+            salary: 'Maosh',
+            invest: 'Investitsiya',
+            tools: 'O\'quv qurollari',
+            taxes: 'Soliqlar',
+            operatingExpenses: 'Xo\'jalik xarajatlari'
+        },
+        salary: {
+            salaryTitle: 'ISH HAQI',
+            settingsSalaryCalculator: 'Ish haqi hisoblagichini sozlash'
+        },
+        firstSettingsSalary: {
+            firstSettingsSalaryTitle: '1. Barcha o\'qituvchilar uchun standart xarajatlarni belgilash parametrlarini ko\'rsating.',
+            optionExpances: 'Xarajat qiymati',
+            cash: 'Naqd',
+            save: 'Saqlash'
+        },
+        secondSettingsSalary: {
+            secondSettingsSalaryTitle: '2. Ba\'zi o\'qituvchilar uchun individual hisoblashingiz mumkin.',
+            chooseTeacher: 'O\'qituvchini tanlash',
+            choose: 'Tanlash',
+            optionExpances: 'Xarajat qiymati',
+            cash: 'Naqd',
+            save: 'Saqlash',
+            teacher: 'O\'qituvchi',
+            methodCount: 'Hisoblash usuli',
+            byDate: 'Sana bo\'yicha',
+            filter: 'Sana bo\'yicha',
+            teacher: 'O\'qituvchi',
+            currency: 'So\'m'
+        },
+        paymentGroup: {
+            paymentGroupTitle: 'TO\'LOV GURUHI',
+            incomeForPeriod: 'Davr uchun tushumlar',
+            showAmount: '(sana va miqdor ko\'rsatiladi)',
+            id: 'ID',
+            startDay: 'Sanadan boshlab',
+            amount: 'Miqdori',
+            paymentType: 'Pul o\'tkazish turi',
+            currency: 'So\'m'
+        },
+        orderInfoGroup: {
+            orderInfoTitle: 'Buyurtma ma\'lumotlari',
+            groups: 'Guruhlar',
+            teachers: 'O\'qituvchilar',
+            courses: 'Kurslar',
+            currency: 'So\'m'
+        },
+        paymentCourses: {
+            paymentCoursesTitle: 'KURS TO\'LOVLARI',
+            incomeForPeriod: 'Davr uchun tushumlar: (sana va miqdor ko\'rsatiladi)',
+            id: 'ID',
+            startDay: 'Sanadan boshlab',
+            amount: 'Miqdori',
+            typePayment: 'Pul o\'tkazish turi',
+            currency: 'So\'m'
+        },
+        orderInfoCourse: {
+            orderInfoTitle: 'Buyurtma ma\'lumotlari',
+            courses: 'Kurslar',
+            sum: 'Summa',
+            overAll: 'Jami:'
+        }
+
+    },
+    groups: {
+        groupsTitle: 'GURUHLAR',
+        addNewGroup: 'Yangi qo\'shish',
+        activeGroups: 'Faol guruhlar',
+        archivedGroups: 'Arxiv guruhlar',
+        teacherNames: 'O\'qituvchilar nomlari',
+        tableRow: {
+            code: 'Kod',
+            courseName: 'Kurs nomi',
+            teacher: 'O\'qituvchi',
+            days: ['Dushanba', 'Seshanba', 'Chorshanba', 'Payshanba', 'Juma', 'Shanba', 'Yakshanba'],
+            daysShort: ['Du', 'Se', 'Chor', 'Pay', 'Ju', 'Shan', 'Yak'],
+            trainingDates: 'Mashg\'ulot sanalari',
+            room: 'Xona',
+            countStudents: 'Talabalar soni'
+        },
+        addNewGroups: {
+            addNewGroupTitle: 'Yangi guruh qo\'shish',
+            name: 'Nomi',
+            course: 'Kurs',
+            selectCourse:'Kursni tanlang',
+            teacher: 'O\'qituvchi',
+            selectTeacher: 'O\'qitivchini tanlang',
+            days: 'Kunlari',
+            selectDays: 'Kunlarini tanlang',
+            room: 'Xona',
+            selectRoom: 'Xonani tanlang',
+            oddDays: 'Toq kunlar',
+            evenDays: 'Juft kunlar',
+            weekends: 'Dam olish kuni',
+            everyDay: 'Har kuni (xaftada 5 kun)',
+            other: 'Boshqa',
+            startTimeLesson: 'Darsning boshlanish vaqti',
+            startGroupDate: 'Guruh boshlanishining sanasi',
+            endGroupDate: 'Guruh tugashining sanasi',
+            save: 'Saqlash'
+        },
+        groupInfo: {
+            groupsGroupsName: 'Guruhlar | Guruh nomi |',
+            groupName: 'Guruh nomi',
+            courseTeacher: 'Kurs | O\'qituvchi F.I.O',
+            price: 'Narx:',
+            time: 'Vaqt:',
+            room: 'Xona:',
+            traningDates: 'Mashg\'ulotlar sanalari:',
+            search: 'Qidirish',
+            attendance: 'Davomat',
+            onlineLessonMaterials: 'Online dars va materiallar',
+            costs: 'Chegirmalar',
+            history: 'Tarix',
+            courseNotStarted: 'Kurs boshlanmagan'
+        },
+        months: {
+            monthsTitle: ['Yanvar', 'Fevral', 'Mart', 'Aprel', 'May','Iyun', 'Iyul', 'Avgust', 'Sentabr', 'Oktabr', 'Noyabr', 'Dekabr']
+        },
+        attendance: {
+            attendanceTitle: 'Davomat',
+            fullName: 'F.I.O'
+        },
+        studentsCanUseMaterials: 'Dars materiallaridan o\'quvchilar foydalanishlari mumkin.',
+        youCanDownloadMaterials: 'Kurs sahifasida dars materiallarini yuklashingiz mumkin.',
+        materialsDontDownloadedYet: 'Dars va materiallar hali yuklanmagan.',
+        individualPrice: {
+            individualPriceTitle: 'Individual narx',
+            everyStudentPrice: 'Har bir o\'quvchi uchun alohida narx belgilashingiz mumkin. Chegirmali narxni kiriting va "Enter" tugmasini bosing.',
+            search: 'Qidiruv',
+            fullName: 'F.I.O',
+            phoneNumber: 'Telefon raqami',
+            individualPrice: 'Individual narx'
+        },
+        groupEdited: 'Guruh tahrirlangan',
+        newStudent: 'Yangi talaba',
+        selectStudent: 'Talabani tanlang',
+        searchByNameOrNumber: 'Ism yoki telefon raqam orqali topish',
+        comeToCourse: 'Kursga kelgan sanasi',
+        date: 'kun/oy/yil',
+        send: 'Yuborish',
+        additionalOption: {
+            freeze: 'Muzlatish',
+            payment: 'To\'lov',
+            addNewNote: 'Yangi eslatma qo\'shish',
+            addToGroupStudent: 'Talabani guruhga qo\'shish',
+            changeGroup: 'Guruhga boshqa guruhga o\'tkazish'
+        },
+        activate: {
+            activateTitle: 'Faollashtirish',
+            payment: 'To\'lov',
+            addNewNote: 'Yangi eslatma qo\'shish',
+            changeGroupStudent: 'Talabani boshqa guruhga o\'tkazish'
+        },
+        exitToGroup: 'Guruhdan chiqarish',
+        receipt: 'Kvitansiya',
+        paymentInfo: {
+            receiptNumber: 'Chek raqami:',
+            student: 'Talaba:',
+            paymentType: 'To\'lov usuli:',
+            amountPayment: 'To\'lov miqdori:',
+            paymentDate: 'To\'langan sanasi:',
+            currentDate: 'Hozirgi sana:',
+            teacher: 'O\'qituvchi:',
+            group: 'Guruhi:'
+        },
+        print: 'Chop etish'
     }
 }
 

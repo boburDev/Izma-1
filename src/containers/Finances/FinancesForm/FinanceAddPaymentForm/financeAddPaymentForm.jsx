2ab740af5e4e5d787974f6f8cf71f569d16df552
boburDev <91521239+soffit2021@users.noreply.github.com>
2021-10-07 11:34:32 +0500

    1

--- src/containers/Finances/FinancesForm/FinanceAddPaymentForm/financeAddPaymentForm.jsx
+++ src/containers/Finances/FinancesForm/FinanceAddPaymentForm/financeAddPaymentForm.jsx
@@ -8,6 +8,7 @@ import { CHECK_CASH, NEW_CASH, UPDATE_CASH, HISTORY_PAYMENT, STATUS_3_4, STUDENT
 import { useMutation, useQuery, useSubscription } from '@apollo/client'
 import DropSearch from '../../../../components/DropSearch/DropSearch'
 import { useCheck } from '../../../../context/CheckProvider'
+import moment from 'moment'
 
 
 const FinanceAddPaymentForm = ({ onClose, studenID, groupID = '' }) => {

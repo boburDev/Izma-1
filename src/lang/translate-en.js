c01ee960a37c1a23d0c8f4c75a6b4b834561de16
shon_parker <nizamkhujaev@gmail.com>
2021-10-07 10:48:53 +0500

    ended engish language

--- src/lang/translate-en.js
+++ src/lang/translate-en.js
@@ -35,7 +35,530 @@ const en = {
             leads: 'leads',
             waitAndPack: 'Waiting and packing',
             groupDebpt: 'Debt in the group'
+        },
+        journals: {
+            courseStarted: 'Course opened:'
+        },
+        tabels: {
+            oddDays: 'Odd days',
+            evenDays: 'A couple of days',
+            weekends: 'Wekkends',
+            everyDay: 'Every day',
+            others: 'Others '
         }
+    },
+    students: {
+        title: {
+            pageTitle: 'Students'
+        },
+        search: {
+            searchHolder: 'Search by name or phone number'
+        },
+        addNewStudent: {
+            addStudent: 'Add new'
+        },
+        abtCourse: {
+            debt: 'Qarzdorlar',
+            discount: 'Chegirmalar'
+        },
+        id: {
+            fullName: 'F.I.O.',
+            phoneNumber: 'Phone',
+            groupNames: 'Guruhlar'
+        },
+        studentInfoTitle: {
+            students: 'Students ',
+            profile: 'Profile ',
+            menu: 'Menu'
+        },
+        archiveStudents: {
+            archiveStudent: 'Profile History'
+        },
+        studentsInfo: {
+            studentPhoneNumber: 'Phone number:',
+            studentBalance: 'Balance:',
+            role: 'Role:',
+            fillial: 'Branch:'
+        },
+        groups: {
+            groupsTitle: 'Groups',
+            groupsSelected: 'Course is selected'
+        },
+        payment: {
+            paymentTitle: 'PAYMENTS',
+            recordedDate: 'Recorded date',
+            paymentId: 'ID',
+            paymentType: 'Payment method',
+            paymentDate: 'Sana',
+            debt: 'Debt',
+            credit: 'Credit',
+            comment: 'Note',
+            employee: 'Employee'
+        },
+        background: {
+            data: 'No data',
+            addNewNote: 'Add a new note',
+            comment: 'Note',
+            save: 'Save'
+        },
+        editStudentInfo: {
+            editStudentInfoTitle: 'EDITING STUDENT DATA',
+            phoneNumber: 'Phone number',
+            fullName: 'F.I.O',
+            birthday: 'Date of birth',
+            gender: ['Male ', 'Female'],
+            comment: 'Note',
+            secondaryPhone: 'Secondary telephone',
+            parentPhoneNumber: 'Parent\'s phone number',
+            telegramUsername: 'Telegram (@username)',
+            password: 'Password',
+            edit: 'Change'
+        },
+        sendMessageToStudent: {
+            message: 'Not activated for your center!'
+        },
+        recordPayment: {
+            recordPaymentTitle: 'Record payments',
+            fullName: 'F.I.O',
+            paymentType: 'Type of payment:',
+            cash: 'Cash',
+            card: 'UzCard',
+            bankAccount: 'Through a bank account',
+            acceptedPaymentValue: 'Amount of money received:',
+            groupName: 'Group',
+            AcceptedDate: 'Date of receipt:',
+            date: 'Day / Month / Year',
+            comment: 'Note',
+            failedPayment: 'RETURN PAYMENT',
+            amount: 'Amount:',
+            save: 'Save'
+        },
+        addedStudentToGroup: {
+            addedStudentToGroupTitle: 'ADD A STUDENT TO THE GROUP',
+            addedDate: 'Date Added:',
+            save: 'Save',
+            payToAccount: 'Account replenished:',
+            editedAction: 'Changed Status: Inactive (Test Lesson)'
+        },
+        addNewStudentTitle: {
+            addNewStudentTitle: 'ADD A NEW STUDENT',
+            phoneNumber: 'ADD A NEW STUDENT',
+            fullName: 'F.I.O',
+            birthday: 'Date of birth',
+            gender: ['Male ', 'Female'],
+            selectGroup: 'Select a group',
+            comment: 'Note',
+            secondaryPhone: 'Secondary telephone',
+            parentPhoneNumber: 'Parent\'s phone number',
+            telegramUsername: 'Telegram (@username)',
+            password: 'Password',
+            save: 'Save'
+        }
+    },
+    teachers: {
+        teacher: {
+            teacherTitle: 'TEACHERS'
+        },
+        teacherInfo: {
+            teacherId: 'ID',
+            fullName: 'F.I.O',
+            phoneNumber: 'Phone number'
+        }, 
+        addNewUser: {
+            phoneNumber: 'Phone number',
+            fullName: 'F.I.O',
+            birthday: 'Date of birth',
+            gender: ['Male ', 'Female'],
+            comment: 'Note',
+            photo: 'Photo',
+            password: 'Password',
+            save: 'Save'
+        },
+        teacherInfo: {
+            teacherName: 'Teacher\'s Name',
+            archiveProfile: 'PROFILE HISTORY',
+            phoneNumber: 'Phone number:',
+            birthday: 'Date of birth:',
+            role: 'Role:',
+            fillial: 'Branch:'
+        },
+        groups: {
+            groupName: 'Group name',
+            groupdCode: 'Code',
+            date: 'Date, time.'
+        },
+        courses: {
+            courseName: 'Course Name',
+            startDay: 'Start: (course time)',
+            profileHistory: 'Profile History',
+            accountReplenished: 'Account replenished:',
+            editSituation: 'Account replenished:'
+        },
+        editTeacherInfo: {
+            editTeacherInfoTitle: 'EDITING TEACHER DATA',
+            phoneNumber: 'Phone number',
+            fullName: 'F.I.O',
+            birthday: 'Date of birth',
+            gender: ['Male ', 'Female'],
+            comment: 'Note',
+            photo: 'Photo',
+            fileNotChoosen: 'No files were selected',
+            download: 'Download',
+            password: 'Password',
+            edit: 'Change'
+        }
+    },
+    settings: {
+        map: {
+            mapTitle: 'ROADMAP',
+            overallBall: 'Total score',
+            highestBall: 'High score',
+            yourBall: 'Your score',
+            section: 'Section',
+            questions: 'Questions',
+            highestScore: 'Score (high)',
+            result: 'The result'
+        },
+        employee: {
+            employeeTitle: 'EMPLOYEES',
+            addNewEmployee: 'Add a new employee',
+            id: 'ID',
+            fullName: 'F.I.O',
+            role: 'Position',
+            phoneNumber: 'Position',
+            edit: 'Edit',
+            actions: 'Actions'
+        },
+        editable: {
+            editableTitle: 'EDIT',
+            phoneNumber: 'Phone number',
+            fullName: 'F.I.O',
+            birthday: 'Date of birth',
+            gender: ['Male', 'Woman'],
+            photo: 'Photo',
+            photoNotChoosen: 'No images were selected',
+            download: 'Download',
+            password: 'Password',
+            leader: 'Leader',
+            adminstrator: 'Adminstrator',
+            teacher: 'Teacher',
+            marketolog: 'Marketer',
+            accountant: 'Accountant',
+            save: 'Save'
+        },
+        rooms: {
+            roomsTitle: 'ROOMS',
+            addNewRoom: 'Add a new room',
+            id: 'ID',
+            fullName: 'F.I.O',
+            edit: 'Edit',
+            actions: 'Actions',
+            roomName: 'Room name',
+            save: 'Save'
+        },
+        journals: {
+            journalsTitle: 'JOURNALS'
+        },
+        forms: {
+            formTitle: 'FORMS',
+            addNewForm: 'Create a new form',
+            id: 'ID',
+            fullName: 'F.I.O',
+            role: 'Position',
+            phoneNumber: 'Phone number',
+            actions: 'Actions',
+            messageForLc: 'Leave your questions and inquiries to our center.',
+            happyWhatWelistening: 'Glad to hear from you!',
+            surname: 'Surname',
+            name: 'Name',
+            phoneNum: 'Phone number',
+            comment: 'Note',
+            selectCourse: 'Choose a course',
+            send: 'Send',
+            commentedForLc: 'You can leave your requests to the Learning Center.',
+            enter: 'Introduction',
+            numberPhone: 'Phone number',
+            password: 'Password',
+            download: 'Download',
+            enterence: 'Introduction'
+        },
+        companySettings: {
+            companySettingsTitle: 'COMPANY SETTINGS',
+            logo: 'Logo',
+            chooseMainColor: 'Select the base color',
+            lcName: 'Name of the training cepictu',
+            full: 'Full',
+            bgcForPage: 'Picture for page background',
+            download: 'Download',
+            mainPhoto: 'The main picture',
+            suggestions: 'Organizational suggestions',
+            paymentTypeForStudent: 'Payment mode for student',
+            plasticCard: 'Plastic card',
+            cash: 'Cash',
+            acrossBankAccount: 'Through a bank account',
+            facebookPixel: 'Facebook pixels',
+            paynet: 'Paynet',
+            paycomUserId: 'Paycom merchant ID number',
+            paycomeUsername: 'Paycom username',
+            paycomPassword: 'Paycom password',
+            save: 'Save'
+        }
+    },
+    courses: {
+        courseTitle: 'COURSES',
+        addCourse: 'Add new',
+        editCourse: {
+            editCourseTitle: 'COURSE EDITING',
+            name: 'Name',
+            price: 'Cost',
+            description: 'Description',
+            create: 'Create'
+        },
+        courseName: {
+            courseNameTitle: 'COURSE NAME',
+            onlineLessonMaterials: 'Online lessons and materials',
+            degrees: 'Levels',
+            groups: 'Groups'
+        },
+        addLesson: {
+            addLessonTitle: 'Add a lesson',
+        },
+        degrees: {
+            degreesTitle: 'Levels',
+            addDegree: 'Add a level',
+            notDegree: 'Levels are not included yet.'
+        }
+    },
+    navigation: {
+        main: 'Home',
+        lids: 'Lids',
+        students: 'Students',
+        teachers: 'Teachers',
+        courses: 'Courses ',
+        finance: 'Finance',
+        settings: 'Settings',
+        allPayments: 'Payments',
+        costs: 'Costs',
+        salaryForWork: 'Salary',
+        paymentGroups: 'Payment group ',
+        paymentCourses: 'Course fees',
+        map: 'Map',
+        office: 'Office',
+        others: 'Others',
+        shapes: 'Forms',
+        companySettings: 'Company settings'
+    },
+    finance: {
+        financeTitle: 'FINANCE (All students)',
+        countStudentOverSum: 'Total on students\' accounts: soums',
+        receiptsForPeriod: 'Revenues for the period:',
+        currency: 'soums',
+        startDay: 'From date',
+        endDay: 'to date',
+        filter: {
+            filterTitle: 'Filter',
+            studentsFullName: 'Students F.I.O.',
+            phoneNumber: 'Phone number',
+            typePayment: 'Type of payment',
+            currency: 'Soum',
+            comment: 'Note'
+        },
+        costs: {
+            costsTitle: 'COSTS',
+            startDay: 'From date',
+            endDay: 'to date',
+            filter: 'Filter'
+        },
+        costsForPeriod : {
+            costsForPeriodTitle: 'Expenses for the period',
+            name: 'Name',
+            date: 'Date',
+            type: 'Category',
+            recipient: 'Recipient',
+            currency: 'Soum',
+            actions: 'Actions'
+        },
+        newCosts: {
+            newCostsTitle: 'New costs',
+            name: 'Name',
+            recievedDate: 'Date of receipt',
+            yymmdd: 'day / month / year',
+            type: 'Category',
+            others: 'Others',
+            recipient: 'Recipient',
+            currency: 'Soum',
+            save: 'Save'
+        },
+        select: {
+            selectTitle: 'Selection',
+            administrativeExpenses: 'Administrative expenses',
+            rent: 'Rent',
+            salary: 'Salary',
+            invest: 'Investment',
+            tools: 'Training tools',
+            taxes: 'Taxes',
+            operatingExpenses: 'Operating expenses'
+        },
+        salary: {
+            salaryTitle: 'SALARY',
+            settingsSalaryCalculator: 'Setting up a payroll counter'
+        },
+        firstSettingsSalary: {
+            firstSettingsSalaryTitle: '1. Specify the standard cost setting parameters for all teachers.',
+            optionExpances: 'Cost value',
+            cash: 'Cash',
+            save: 'Save'
+        },
+        secondSettingsSalary: {
+            secondSettingsSalaryTitle: '2. You can calculate individually for some teachers.',
+            chooseTeacher: 'Teacher selection',
+            choose: 'Selection',
+            optionExpances: 'Cost value',
+            cash: 'Cash',
+            save: 'Save',
+            teacher: 'Teacher',
+            methodCount: 'Calculation method',
+            byDate: 'By date',
+            filter: 'Filter',
+            teacher: 'Teacher',
+            currency: 'Soum'
+        },
+        paymentGroup: {
+            paymentGroupTitle: 'PAYMENT GROUP',
+            incomeForPeriod: 'Revenues for the period',
+            showAmount: '(date and quantity indicated)',
+            id: 'ID',
+            startDay: 'From the date',
+            amount: 'Quantity',
+            paymentType: 'Type of money transfer',
+            currency: 'Soum'
+        },
+        orderInfoGroup: {
+            orderInfoTitle: 'Order information',
+            groups: 'Groups',
+            teachers: 'Teachers',
+            courses: 'Courses',
+            currency: 'Soum'
+        },
+        paymentCourses: {
+            paymentCoursesTitle: 'COURSE FEES',
+            incomeForPeriod: 'Receipts for the period: (date and amount are indicated)',
+            id: 'ID',
+            startDay: 'From the date',
+            amount: 'Quantity',
+            typePayment: 'Type of money transfer',
+            currency: 'Soum'
+        },
+        orderInfoCourse: {
+            orderInfoTitle: 'Order information',
+            courses: 'Courses',
+            sum: 'Amount',
+            overAll: 'Total:'
+        }
+    },
+    groups: {
+        groupsTitle: 'GROUPS',
+        addNewGroup: 'Add new',
+        activeGroups: 'Active groups',
+        archivedGroups: 'Archive groups',
+        teacherNames: 'Names of teachers',
+        tableRow: {
+            code: 'Code',
+            courseName: 'Course name',
+            teacher: 'Teacher',
+            days: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+            daysShort: ['Mon', 'Tu', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+            trainingDates: 'Course dates',
+            room: 'Room',
+            countStudents: 'Number of students'
+        },
+        addNewGroups: {
+            addNewGroupTitle: 'Add a new group',
+            name: 'Name',
+            course: 'Course',
+            selectCourse:'Choose a course',
+            teacher: 'Teacher',
+            selectTeacher: 'Choose a teacher',
+            days: 'Days',
+            selectDays: 'Select dates',
+            room: 'Room',
+            selectRoom: 'Select room',
+            oddDays: 'Odd day',
+            evenDays: 'A couple of days',
+            weekends: 'Weekends',
+            everyDay: 'Every day (5 days a week)',
+            other: 'Other',
+            startTimeLesson: 'Lesson start time',
+            startGroupDate: 'Date of group start',
+            endGroupDate: 'Date of group start',
+            save: 'Save'
+        },
+        groupInfo: {
+            groupsGroupsName: 'Groups | Group name |',
+            groupName: 'Group name',
+            courseTeacher: 'Course | Teacher F.I.O.',
+            price: 'Price:',
+            time: 'Time:',
+            room: 'Room:',
+            traningDates: 'Class dates:',
+            search: 'Search',
+            attendance: 'Attendance',
+            onlineLessonMaterials: 'Online lessons and materials',
+            costs: 'Discounts',
+            history: 'History',
+            courseNotStarted: 'The course has not started'
+        },
+        months: {
+            monthsTitle: ['January', 'February', 'March', 'April', 'May','June', 'July', 'August', 'September', 'October', 'November', 'December']
+        },
+        attendance: {
+            attendanceTitle: 'Attendance',
+            fullName: 'F.I.O'
+        },
+        studentsCanUseMaterials: 'Lesson materials can be used by students.',
+        youCanDownloadMaterials: 'You can download course materials on the course page.',
+        materialsDontDownloadedYet: 'Lessons and materials have not yet been uploaded.',
+        individualPrice: {
+            individualPriceTitle: 'Individual price',
+            everyStudentPrice: 'You can set a separate mark for each student. Enter the discounted price and press Enter.',
+            search: 'Search',
+            fullName: 'F.I.O',
+            phoneNumber: 'Phone number',
+            individualPrice: 'Individual price'
+        },
+        groupEdited: 'The group has been edited',
+        newStudent: 'New student',
+        selectStudent: 'Select the student',
+        searchByNameOrNumber: 'Find by name or phone number',
+        comeToCourse: 'Date of arrival of the course',
+        date: 'day / month / year',
+        send: 'Send',
+        additionalOption: {
+            freeze: 'Freezing',
+            payment: 'Payment',
+            addNewNote: 'Add a new note',
+            addToGroupStudent: 'Add a student to a group',
+            changeGroup: 'Transfer to another group'
+        },
+        activate: {
+            activateTitle: 'Activation',
+            payment: 'Payment',
+            addNewNote: 'Add a new note',
+            changeGroupStudent: 'Transfer the student to another group'
+        },
+        exitToGroup: 'Removal from the group',
+        receipt: 'Receipt',
+        paymentInfo: {
+            receiptNumber: 'Check number:',
+            student: 'Student:',
+            paymentType: 'Payment method:',
+            amountPayment: 'Payment amount:',
+            paymentDate: 'Date of payment:',
+            currentDate: 'Current date:',
+            teacher: 'Teacher:',
+            group: 'Group:'
+        },
+        print: 'Print out'
     }
 }
 
